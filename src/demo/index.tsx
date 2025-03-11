ff0129cdddf6946d46545184b7cea78cbb33e21e
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-03-11 21:22:19 +0800

    feat: download demo md

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
+import { stream } from "hono/streaming";
 import { Suspense } from "hono/jsx/streaming";
 import demoData from "./data.json";
 import { Ocr2html } from "../../components/ocr2html";
 import { Nav } from "../../components/nav";
 import { Footer } from "../../components/footer";
+import { createTar } from "../../utils/createTar";
 
 const demo = new Hono().basePath("/demo");
 
@@ -14,9 +16,15 @@ demo.get("/", async (c) => {
         <Nav />
         <h1>DEMO</h1>
         <p>
+          You can find original paper{" "}
           <a target="_blank" href="https://arxiv.org/pdf/2201.04234">
-            You can find original paper here.
+            here
+          </a>{" "}
+          and download raw markdown{" "}
+          <a href="/demo/md" download="demo.tar">
+            here
           </a>
+          .
         </p>
       </header>
       <main>
@@ -27,6 +35,16 @@ demo.get("/", async (c) => {
       <Footer />
     </>
   );
+});
+
+demo.get("/md", async (c) => {
+  const id = "demo";
+  c.header("Content-Disposition", `attachment; filename="${id}.tar"`);
+
+  const tar = createTar(demoData, id);
+  return stream(c, async (stream) => {
+    await stream.pipe(tar);
+  });
 });
 
 export default demo;
