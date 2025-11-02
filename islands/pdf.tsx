bab962854c33214ec4680506f7afcce19301ca11
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2025-11-02 19:06:38 +0800

    fix: remove ocring component

--- islands/pdf.tsx
+++ islands/pdf.tsx
@@ -9,7 +9,19 @@ export function PDF(props: {
   resp: Signal<OCRResponse | null>;
 }) {
   if (!props.resp.value) {
-    return <OCRing />;
+    return (
+      <>
+        <header>
+          <Nav />
+          <h1>PDF To Markdown</h1>
+        </header>
+        <main>
+          <p class="notice">
+            It seems that it cannot be parsed. Is the English paper you gave?
+          </p>
+        </main>
+      </>
+    );
   }
 
   return (
@@ -25,22 +37,6 @@ export function PDF(props: {
       </header>
       <main>
         <Ocr2html ocrResponse={props.resp.value} />
-      </main>
-    </>
-  );
-}
-
-function OCRing() {
-  return (
-    <>
-      <header>
-        <Nav />
-        <h1>PDF To Markdown</h1>
-      </header>
-      <main>
-        <p class="notice">
-          It seems that it cannot be parsed. Is the English paper you gave?
-        </p>
       </main>
     </>
   );
